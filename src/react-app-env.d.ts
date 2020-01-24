9257f9dca0accca3f9e268e58e22a64e03bfc1af
Nicolás Kivatinetz <nkivatinetz@truelogic.software>
2020-01-23 23:24:04 -0300

    env

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
+    REACT_APP_GET_POPULATION_ENDPOINT: string;
+  }
+}
